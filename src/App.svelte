20ddfbea3366d4b372bad1bfecd00fac4a3f2043
andreitrinidad <andreitrinidad.dev@gmail.com>
2020-04-04 23:02:40 +0800

    ✨ Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	let name = 'Andrei';
+	let dingdongSize = 'small'
+
+	const handleDingdong = () => {
+		dingdongSize = 'large';
+	}
+
+	const handleInput = (e) => {
+		dingdongSize = e.target.value;
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<!-- WHAT SORCERY IS THIS -->
+	<p style="color: {dingdongSize}">Hey it's ya boi andrei, your dingdong is {dingdongSize}</p>
+	<input type="text" bind:value={dingdongSize}>
+	<button on:click={handleDingdong}>No my dingdong is LARGE!!!</button>
+</main>
+
+<style>
+	main {	
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
